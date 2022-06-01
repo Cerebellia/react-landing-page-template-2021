eb179b5398a189700fca21f7067a56f94ec273de
Andreas Ink <andreasink17@gmail.com>
2022-06-01 18:51:21 -0400

    Slight edits

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,10 +9,24 @@ import LazyShow from '../components/LazyShow';
 import MainHero from '../components/MainHero';
 import MainHeroImage from '../components/MainHeroImage';
 import Product from '../components/Product';
+import Head from "next/head";
 
 const App = () => {
   return (
+  
     <div className={`bg-background grid gap-y-16 overflow-hidden`}>
+        <Head>
+    
+    <meta name="keywords" content="Parkinson's Disease, Parkinson's, Tremor, Health" />
+    <meta name="author" content="Cerebellia" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+<meta property="og:title" content="Cerebellia"/>
+<meta property="og:description" content="A company dedicated to the wellbeing of individuals afflicted with Parkinson's Disease"/>
+<meta property="og:image" content="https://cerebellia.com/assets/images/landing.png"/>
+<meta name="twitter:card" content="summary_large_image"></meta>
+
+  </Head>
       <div className={`relative bg-background`}>
         <div className="max-w-7xl mx-auto">
           <div
--- src/utils/AppConfig.ts
+++ src/utils/AppConfig.ts
@@ -1,6 +1,6 @@
 export const AppConfig = {
   site_name: 'Cerebellia',
-  title: 'a company dedicated to the wellbeing of individuals afflicted with Parkinson\'s Disease ',
+  title: 'A company dedicated to the wellbeing of individuals afflicted with Parkinson\'s Disease ',
   description: 'Track, Understand and Optimize your Parkinson\'s Disease Symptoms',
   locale: 'en',
 };
